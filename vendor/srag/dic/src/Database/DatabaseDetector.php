6d387bc3d5e1dccb226d0f6764e1350c618518a5
Mirco Hilbert <mhilbert@users.noreply.github.com>
2023-09-29 11:40:47 +0200

    function primaryExistsByFields() added
    solves:
    
    Whoops\Exception\ErrorException thrown with message "Class srag\DIC\OpencastPageComponent\Database\DatabaseDetector contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (ilDBInterface::primaryExistsByFields)"

--- vendor/srag/dic/src/Database/DatabaseDetector.php
+++ vendor/srag/dic/src/Database/DatabaseDetector.php
@@ -216,4 +216,11 @@ class DatabaseDetector extends AbstractILIASDatabaseDetector
             return $primary_key_value;
         }
     }
+
+
+    public function primaryExistsByFields(string $table_name, array $fields): bool
+    {
+        // TODO: Implement primaryExistsByFields() method.
+        return false;
+    }
 }
